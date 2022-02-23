5566a95acf180c0a2862ac825cb466d015f52e35
MathiasMeisner <62882868+MathiasMeisner@users.noreply.github.com>
2022-02-23 15:55:45 +0100

    Added PricesController
    GetAll and GetById http request

---
+++ PriceValuationFeature/Controllers/PricesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PriceValuationFeature.Models;
+using PriceValuationFeature.Managers;
+
+namespace PriceValuationFeature.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PricesController : ControllerBase
+    {
+        private readonly PriceValuationFeatureManager _manager = new PriceValuationFeatureManager();
+
+        // GET: api/<PricesController>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public List<Home> GetAll([FromQuery] string sortBy)
+        {
+            List<Home> homes = _manager.GetAll(sortBy);
+            return homes;
+        }
+
+        // GET: PricesController/Details/5
+        [HttpGet("{id}")]
+        public ActionResult<Home> GetById(int id)
+        {
+            Home home = _manager.GetById(id);
+            if (home == null) return NotFound("No id: " + id);
+            return home;
+        }
+    }
+}
