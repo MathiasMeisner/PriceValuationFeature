38b76366f5dcd9cb68275a1ca93fe6426e4eb99b
MathiasMeisner <62882868+MathiasMeisner@users.noreply.github.com>
2022-02-23 15:29:09 +0100

    Added PriceValuationFeatureManager and Xunit Test for manager class.
    The manager includes relevant http methods and calculation functions for the price feature.

---
+++ PriceValuationFeature/Managers/PriceValuationFeatureManager.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PriceValuationFeature.Models;
+
+namespace PriceValuationFeature.Managers
+{
+    public class PriceValuationFeatureManager
+    {
+        private static int _nextId = 1;
+
+        private static List<Home> homes = new List<Home>()
+        {
+            new Home(){Id = _nextId++, MunicipalityId = 1, IsOpen = true, Price = 6000000, Kvm = 150, DaysForSale = 180},
+            new Home(){Id = _nextId++, MunicipalityId = 1, IsOpen = false, Price = 4000000,  Kvm = 100, DaysForSale = 127},
+            new Home(){Id = _nextId++, MunicipalityId = 1, IsOpen = false, Price = 5000000,  Kvm = 75, DaysForSale = 60},
+            new Home(){Id = _nextId++, MunicipalityId = 2, IsOpen = false, Price = 3000000,  Kvm = 125, DaysForSale = 210},
+            new Home(){Id = _nextId++, MunicipalityId = 2, IsOpen = true, Price = 1000000,  Kvm = 75, DaysForSale = 50},
+            new Home(){Id = _nextId++, MunicipalityId = 2, IsOpen = true, Price = 2750000,  Kvm = 115, DaysForSale = 70}
+        };
+
+        private static List<Municipality> municipalities = new List<Municipality>()
+        {
+            new Municipality(){Id = 1, Name = "Frederiksberg"},
+            new Municipality(){Id = 2, Name = "Roskilde"}
+        };
+
+        public List<Home> GetAll(string sortBy = null)
+        {
+            List<Home> homesList = new List<Home>(homes);
+            if (sortBy != null)
+            {
+                switch (sortBy)
+                {
+                    case "price":
+                        homesList = homesList.OrderByDescending(home => home.Price).ToList();
+                        break;
+                    case "kvm":
+                        homesList = homesList.OrderByDescending(home => home.Kvm).ToList();
+                        break;
+                    case "daysforsale":
+                        homesList = homesList.OrderByDescending(home => home.DaysForSale).ToList();
+                        break;
+                }
+            }
+            return homesList;
+        }
+
+        public Home GetById(int id)
+        {
+            return homes.Find(home => home.Id == id);
+        }
+
+        public int TotalHomesForSale()
+        {
+            return homes.Count(x => x.IsOpen);
+        }
+
+        public double AvgKvmPriceInMunicipality()
+        {
+            double d = homes.Where(x => x.MunicipalityId == 1).Select(x => x.AvgKvmPrice()).Average();
+            double dc = Math.Round((double)d, 0);
+            return dc;
+        }
+
+        public double AvgKvmPriceInMunicipality2()
+        {
+            double d = homes.Where(x => x.MunicipalityId == 2).Select(x => x.AvgKvmPrice()).Average();
+            double dc = Math.Round((double)d, 0);
+            return dc;
+        }
+
+        public double AvgDaysForSaleInMunicipality()
+        {
+            return homes.Where(x => x.MunicipalityId == 1 && x.IsOpen == false).Select(x => x.DaysForSale).Average();
+        }
+    }
+}
---
+++ PriceValuationFeatureTests/ManagerTests.cs
@@ -0,0 +1,75 @@
+using System;
+using Xunit;
+using PriceValuationFeature.Managers;
+using System.Collections.Generic;
+using PriceValuationFeature.Models;
+
+namespace PriceValuationFeatureTests
+{
+    public class ManagerTests
+    {
+        private readonly PriceValuationFeatureManager _manager = new PriceValuationFeatureManager();
+
+        [Fact]
+        public void GetAllTest_ShouldPass()
+        {
+            List<Home> homes = _manager.GetAll();
+
+            Assert.Equal(6, homes.Count);
+        }
+
+        [Fact]
+        public void GetByIdTest()
+        {
+            Home home = _manager.GetById(1);
+
+            Assert.Equal(1, home.Id);
+        }
+
+        [Fact]
+        public void TotalHomesForSaleTest()
+        {
+            Assert.Equal(3, _manager.TotalHomesForSale());
+        }
+
+        [Fact]
+        public void AvgKvmPriceTest()
+        {
+            // ARRANGE - define a variable showing the expected results with decimals
+            double d = 48888.8888;
+
+            // ACT - use math round to remove decimals
+            double dc = Math.Round((double)d, 0);
+
+            // ASSERT - takes the new variable and compares it to the method outcome
+            Assert.Equal(dc, _manager.AvgKvmPriceInMunicipality());
+        }
+
+        [Fact]
+        public void AvgKvmPriceTest2()
+        {
+            double d = 20415.4589;
+            double dc = Math.Round((double)d, 0);
+            Assert.Equal(dc, _manager.AvgKvmPriceInMunicipality2());
+        }
+
+        [Fact]
+        public void AvgDaysForSaleTest()
+        {
+            Assert.Equal(93.5, _manager.AvgDaysForSaleInMunicipality());
+        }
+
+        [Fact]
+        public void SortTest()
+        {
+            List<Home> sortPrice = _manager.GetAll(sortBy: "price");
+            Assert.Equal(6000000, sortPrice[0].Price);
+
+            List<Home> sortKvm = _manager.GetAll(sortBy: "kvm");
+            Assert.Equal(115, sortKvm[2].Kvm);
+
+            List<Home> sortDaysForSale = _manager.GetAll(sortBy: "daysforsale");
+            Assert.Equal(180, sortDaysForSale[1].DaysForSale);
+        }
+    }
+}
