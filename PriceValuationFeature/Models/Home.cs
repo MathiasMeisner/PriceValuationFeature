3b7780a58ec137c5edded33df6db5a3ec4866391
MathiasMeisner <62882868+MathiasMeisner@users.noreply.github.com>
2022-02-23 15:13:28 +0100

    Added home.cs and municipality.cs Model classes with properties

---
+++ PriceValuationFeature/Models/Home.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PriceValuationFeature.Models
+{
+    public class Home
+    {
+        private int _id;
+
+        public int Id
+        {
+            get => _id;
+            set
+            {
+                _id = value;
+            }
+        }
+
+        private int _municipalityId;
+
+        public int MunicipalityId
+        {
+            get => _municipalityId;
+            set
+            {
+                _municipalityId = value;
+            }
+        }
+
+        private bool _isOpen;
+
+        public bool IsOpen
+        {
+            get => _isOpen;
+            set
+            {
+                _isOpen = value;
+            }
+        }
+
+        private int _price;
+
+        public int Price
+        {
+            get => _price;
+            set
+            {
+                _price = value;
+            }
+        }
+
+        private int _kvm;
+
+        public int Kvm
+        {
+            get => _kvm;
+            set
+            {
+                _kvm = value;
+            }
+        }
+
+        private int _daysForSale;
+
+        public int DaysForSale
+        {
+            get => _daysForSale;
+            set
+            {
+                _daysForSale = value;
+            }
+        }
+
+        public int AvgKvmPrice()
+        {
+            return (Price / Kvm);
+        }
+    }
+}
---
+++ PriceValuationFeature/Models/Municipality.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PriceValuationFeature.Models
+{
+    public class Municipality
+    {
+        private int _id;
+
+        public int Id
+        {
+            get => _id;
+            set
+            {
+                _id = value;
+            }
+        }
+
+        private string _name;
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+            }
+        }
+    }
+}
